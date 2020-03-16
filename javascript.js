89eaf0ce959ab978a617f4e6d63166384331a85f
V3KTOR <61921830+TH3V3KTOR@users.noreply.github.com>
2020-03-16 18:38:39 -0500

    creando javascript
    es otro archivo de un javascript

---
+++ javascript.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        Body {color:black;
+         background:cyan;}
+         Em{ color:chartreuse}
+    </style>
+    <title>JAVASCRIPT</title>
+</head>
+<body>
+    <h1>EL JAVASCRIPT</h1>
+<p>HACE UN RATITO SALIO LA VENTANA EMERGENTE</p>
+
+<script>
+window.alert("Mi nombre es: Víctor Galarza");
+</script>
+</body>
+<footer>
+    <form method="GET" action="index.html"><!--llevara a la pagina principal mediante el metodo get, usando submit de envio de datos-->
+        <input type="submit" value="VOLVER AL MENU PRINCIPAL">
+    </form>
+</footer>
+</html>
